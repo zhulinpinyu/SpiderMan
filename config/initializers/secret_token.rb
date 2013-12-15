e462369941e7a216c9fca98d004bdae187ceac49
zhulinpinyu <mulixiang199016@gmail.com>
2013-12-15 23:43:35 +0800

    init commit for SpiderMan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpiderMan::Application.config.secret_key_base = 'de7c8ff9eb51fa645a7ebc842635d4c6ba2b3cec1235f9ecc09fb52bc07840f301b92e3b6b2b35e80c1eef9e845d294bab0864592fde4fb17d4161ff02388f32'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpiderMan::Application.config.session_store :cookie_store, key: '_SpiderMan_session'
